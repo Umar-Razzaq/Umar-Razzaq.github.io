57179a342056bb1ffa440ca8204a1ff78b13eee3
Umar Razzaq <umer9b2@gmail.com>
2020-11-07 02:58:27 +0500

    first commit

---
+++ components/timetablepanel.vue
@@ -0,0 +1,144 @@
+<template>
+  <v-row justify="center">
+    <v-expansion-panels popout>
+      <v-expansion-panel
+        v-for="(route, id) in routes"
+        :key="id"
+        style="margin-bottom: 5px"
+      >
+        <v-expansion-panel-header
+          ><row
+            style="display: flex;
+    justify-content: space-between;"
+            ><h1>{{ route.station }}</h1>
+            <h1 style="font-family: cursive;">{{ route.dtime }}</h1></row
+          ></v-expansion-panel-header
+        >
+        <v-expansion-panel-content>
+          <div
+            style="display: flex;
+    justify-content: space-around;"
+          >
+            <div>
+              <v-icon large color="green darken-2">
+                mdi-map-marker
+              </v-icon>
+              Address: {{ route.address }}
+            </div>
+            <div>
+              <v-icon large color="green darken-2">
+                mdi-clock
+              </v-icon>
+              Estimated time: {{ route.esttime }}
+            </div>
+          </div>
+          <div
+            style="display: flex;
+    justify-content: space-around;"
+          >
+            <div>
+              <v-icon large color="green darken-2">
+                mdi-routes
+              </v-icon>
+              Enroute: {{ route.enroute }}
+            </div>
+            <div>
+              <v-icon large color="green darken-2">
+                mdi-phone
+              </v-icon>
+              Phone: {{ route.phone }}
+            </div>
+          </div>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </v-row>
+</template>
+<script>
+export default {
+  data: () => ({
+    routes: [
+      {
+        station: "Faisal Movers",
+        dtime: "10:00pm",
+        esttime: "12 hrs",
+        address: "Band Rd, Kot Kamboh Lahore, Punjab 54000",
+        phone: "(042) 111 224 488",
+        enroute: "Multan, DGKhan, RJP"
+      },
+      {
+        station: "Daewoo",
+        dtime: "10:00pm",
+        esttime: "12 hrs",
+        address: "Band Rd, Kot Kamboh Lahore, Punjab 54000",
+        phone: "(042) 111 224 488",
+        enroute: "RJP, DGKhan, Multan"
+      },
+      {
+        station: "Faisal Movers",
+        dtime: "10:00pm",
+        esttime: "12 hrs",
+        address: "Band Rd, Kot Kamboh Lahore, Punjab 54000",
+        phone: "(042) 111 224 488",
+        enroute: "Multan, DGKhan, RJP"
+      },
+      {
+        station: "Daewoo",
+        dtime: "10:00pm",
+        esttime: "12 hrs",
+        address: "Band Rd, Kot Kamboh Lahore, Punjab 54000",
+        phone: "(042) 111 224 488",
+        enroute: "RJP, DGKhan, Multan"
+      },
+      {
+        station: "Faisal Movers",
+        dtime: "10:00pm",
+        esttime: "12 hrs",
+        address: "Band Rd, Kot Kamboh Lahore, Punjab 54000",
+        phone: "(042) 111 224 488",
+        enroute: "Multan, DGKhan, RJP"
+      },
+      {
+        station: "Daewoo",
+        dtime: "10:00pm",
+        esttime: "12 hrs",
+        address: "Band Rd, Kot Kamboh Lahore, Punjab 54000",
+        phone: "(042) 111 224 488",
+        enroute: "RJP, DGKhan, Multan"
+      },
+      {
+        station: "Faisal Movers",
+        dtime: "10:00pm",
+        esttime: "12 hrs",
+        address: "Band Rd, Kot Kamboh Lahore, Punjab 54000",
+        phone: "(042) 111 224 488",
+        enroute: "Multan, DGKhan, RJP"
+      },
+      {
+        station: "Daewoo",
+        dtime: "10:00pm",
+        esttime: "12 hrs",
+        address: "Band Rd, Kot Kamboh Lahore, Punjab 54000",
+        phone: "(042) 111 224 488",
+        enroute: "RJP, DGKhan, Multan"
+      },
+      {
+        station: "Faisal Movers",
+        dtime: "10:00pm",
+        esttime: "12 hrs",
+        address: "Band Rd, Kot Kamboh Lahore, Punjab 54000",
+        phone: "(042) 111 224 488",
+        enroute: "Multan, DGKhan, RJP"
+      },
+      {
+        station: "Daewoo",
+        dtime: "10:00pm",
+        esttime: "12 hrs",
+        address: "Band Rd, Kot Kamboh Lahore, Punjab 54000",
+        phone: "(042) 111 224 488",
+        enroute: "RJP, DGKhan, Multan"
+      }
+    ]
+  })
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <v-card elevation="10" light outlined tile>
+      <v-row style="margin: 0px;">
+        <v-col>
+          From
+          <v-autocomplete
+            v-model="from"
+            style="max-width: 70%;"
+            :items="cities"
+            auto-select-first
+            clearable
+          ></v-autocomplete>
+        </v-col>
+        <v-col>
+          To
+          <v-autocomplete
+            v-model="to"
+            style="max-width: 70%;"
+            :items="cities"
+            auto-select-first
+            clearable
+          ></v-autocomplete>
+        </v-col>
+      </v-row>
+    </v-card>
+    <v-card elevation="10" light outlined tile style="margin-top: 10px">
+      <TTPanel v-if="(from !== null) & (to !== null)" />
+    </v-card>
+  </div>
+</template>
+
+<script>
+import TTPanel from "../components/timetablepanel";
+export default {
+  components: {
+    TTPanel
+  },
+  data: () => ({
+    from: null,
+    to: null,
+    cities: ["Lahore", "Karachi", "Rajanpur"]
+  })
+};
+</script>
+
+<style scoped></style>
